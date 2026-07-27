a7df7dd9e01ed89f39558f52b547199fff484e92
Ninh Thiện Luân <luanninh2005@gmail.com>
2026-07-28 00:51:26 +0700

    chore: update default config for translate executor and local llm

--- src/main/java/com/aiwrapper/executor/TranslateExecutor.java
+++ src/main/java/com/aiwrapper/executor/TranslateExecutor.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
